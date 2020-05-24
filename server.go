69bb92319628eb5dcb0ac96c4c2dcfa618e9fd78
Eftal Yurtseven <eftalyurtseven@gmail.com>
2020-05-24 19:00:47 +0300

    delete endpoint

--- server.go
+++ server.go
@@ -7,12 +7,13 @@ import (
 	"log"
 	"net/http" // string convert processes
 	"strconv"
+	"time"
 
 	"github.com/gorilla/mux" // For router
 )
 
 func homeRoute(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome home!")
+	fmt.Fprintf(w, "Welcome strange, you can call /posts path GET,POST,PATCH,DELETE methods! For more information: https://github.com/eftalyurtseven/go-tests")
 }
 
 type post struct {
@@ -33,6 +34,8 @@ var posts = allPosts{
 
 // create a post and append posts variable
 func createPost(w http.ResponseWriter, r *http.Request) {
+	t := time.Now()
+	fmt.Println(t.String() + " - create post called!")
 	var newPost post
 	// get request body and error from r.Body
 	reqBody, err := ioutil.ReadAll(r.Body)
@@ -45,18 +48,21 @@ func createPost(w http.ResponseWriter, r *http.Request) {
 	// append to posts
 	posts = append(posts, newPost)
 	w.WriteHeader(http.StatusCreated)
-
 	json.NewEncoder(w).Encode(newPost)
 }
 
 // get all posts from posts variable
 func getAllPosts(w http.ResponseWriter, r *http.Request) {
+	t := time.Now()
+	fmt.Println(t.String() + " - get all posts called!")
 	// json encode and send header application/json ?
 	json.NewEncoder(w).Encode(posts)
 }
 
 // get single post
 func getOnePost(w http.ResponseWriter, r *http.Request) {
+	t := time.Now()
+	fmt.Println(t.String() + " - get one post called!")
 	// get id
 	postID := mux.Vars(r)["id"]
 	// postID var is string but ID integer my post struct
@@ -81,9 +87,12 @@ func getOnePost(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// update post from ID
 func updatePost(w http.ResponseWriter, r *http.Request) {
 	// get postID from request
 	postID := mux.Vars(r)["id"]
+	t := time.Now()
+	fmt.Println(t.String() + " - update post called by id: " + postID)
 	// postID var is string but ID integer my post struct
 	// convert str to int
 	postIDInt, err := strconv.Atoi(postID)
@@ -117,6 +126,32 @@ func updatePost(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func deletePost(w http.ResponseWriter, r *http.Request) {
+	// get postID from request
+	postID := mux.Vars(r)["id"]
+	t := time.Now()
+	fmt.Println(t.String() + " - delete post called by id: " + postID)
+	// postID var is string but ID integer my post struct
+	// convert str to int
+	postIDInt, err := strconv.Atoi(postID)
+	if err != nil {
+		fmt.Fprintf(w, "String to int casting operation failed :(")
+	}
+	isFind := 0
+
+	for i, singlePost := range posts {
+		if singlePost.ID == postIDInt {
+			isFind = 1
+			// delete finded index :index , index+1:
+			posts = append(posts[:i], posts[i+1:]...)
+			fmt.Fprintf(w, "ID: %v post deleted!", postID)
+		}
+	}
+	if isFind == 0 {
+		fmt.Fprintf(w, postID+" not found in posts!")
+	}
+}
+
 func main() {
 
 	// create a router
@@ -129,9 +164,10 @@ func main() {
 	router.HandleFunc("/posts", getAllPosts).Methods("GET")
 	// for get a single post call getOnePost func in get method
 	router.HandleFunc("/posts/{id}", getOnePost).Methods("GET")
-	// for /posts/{id} path call updatePost func in PATCH method
+	// for /posts/{id} call updatePost func in PATCH method
 	router.HandleFunc("/posts/{id}", updatePost).Methods("PATCH")
-
-	log.Fatal(http.ListenAndServe(":8081", router))
+	// for /posts/{id} path call deletePost func in DELETE method
+	router.HandleFunc("/posts/{id}", deletePost).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8080", router))
 
 }
